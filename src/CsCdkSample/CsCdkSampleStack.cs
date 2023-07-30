9b4a6bffda8aae477be308bc6ec50e5652674f68
pisa <pisa0401mst@gmail.com>
2023-07-30 23:10:22 +0900

    Initial commit

---
+++ src/CsCdkSample/CsCdkSampleStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CsCdkSample
+{
+    public class CsCdkSampleStack : Stack
+    {
+        internal CsCdkSampleStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
